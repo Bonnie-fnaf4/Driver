37ca5a3f3e3e51ae2820ec5164a0f89ec119968c
Bonnie-fnaf4 <103939286+Bonnie-fnaf4@users.noreply.github.com>
2022-04-18 18:57:05 +0600

    Add files via upload

---
+++ Scripts/Camera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public Transform Car; //Переменная для отслеживания положения машины
+
+    public float speed = 4f; //Скорость реагирования камеры на движения машины
+
+    public Vector3 _position; //Переменная позиции
+
+    public float x, y, z;
+    void Start()
+    {
+         _position = Car.InverseTransformPoint(transform.position); //Получаем позицию машины в локальных координатах
+    }
+
+    void Update()
+    {
+        var currentPositin = Car.TransformPoint(_position); //Создаём переменую для координат перемещения камеры
+
+        transform.position = Vector3.Lerp(transform.position, currentPositin, speed * Time.deltaTime); //Перемещаем камеру
+        var currentRotation = Quaternion.LookRotation(Car.position - transform.position); //Рассчитывает траекторию поворота машины
+        transform.rotation = Quaternion.Lerp(transform.rotation, currentRotation, speed * Time.deltaTime); //Присваевает координаты поворота камере
+    }
+}
---
+++ Scripts/GameOver.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    public GameObject Panel, Win, Lost; //переменные для панели
+
+    public Transform Car; //Переменная для проверки координат машины
+
+    public void Start()
+    {
+        Time.timeScale = 1; //устанавливаем нормальное время
+    }
+
+    public void Update()
+    {
+        if(Car.position.y < -5) //Если машина улетела за карту и падает вниз то она проиграла
+        {
+            Red();
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Red") //Если машина коснулась красного объекта то она проиграла
+        {
+            Red();
+        }
+
+        if (other.gameObject.tag == "Green") //Если машина коснулась зелённого объекта то она выиграла
+        {
+            Panel.SetActive(true);
+            Win.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+
+    public void Red() //Войд для проигреша
+    {
+        Panel.SetActive(true);
+        Lost.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void Restart() //Копка перезгрузки
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Exit() //кнопка выхода из игры
+    {
+        Application.Quit();
+    }
+}
